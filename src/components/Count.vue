8cec54034af31ce332bb47b61d9c0f857d0cd778
2316519686 <2316519686@qq.com>
2019-04-08 10:30:49 +0800

    vuex 获取 state object

---
+++ src/components/Count.vue
@@ -0,0 +1,42 @@
+// 访问状态对象
+<template>
+<div>
+    <h3>{{msg}}</h3>
+    <p>{{$store.state.count}}-{{count}}</p>
+     <p>{{count}}</p>
+    <button @click="$store.commit('add')">+</button>
+     <button @click="$store.commit('reduce')">-</button>
+</div>
+</template>
+<script>
+import store from '@/vuex/store'
+//第二种
+import { mapState } from 'vuex'
+export default {
+    data(){
+           return{
+        msg:'Hello vuex'
+    }
+    },
+    //第一种方法：利用计算属性
+    // computed: {
+    //     count(){
+    //         return this.$store.state.count;
+    //     }
+    // },
+
+    ////第二种
+    // computed: mapState({
+    //     count:state=>state.count
+    //     // 等同于
+    //     // count:function(state){
+    //     //     return state.count;
+    //     // }
+    // }),
+
+   // 第三种 利用数组
+   computed:mapState(['count']),
+    store
+ 
+}
+</script>
---
+++ src/vuex/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = { count: 3
+}
+const mutations = {
+  add (state) { state.count++ },
+  reduce (state) { state.count-- }
+
+}
+export default new Vuex.Store({ state, mutations
+})
